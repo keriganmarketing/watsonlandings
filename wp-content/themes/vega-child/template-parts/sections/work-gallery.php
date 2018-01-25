f452de2dd7db520c4e6329be7cb3ad7c66c05967
programmer-man <bryan@kerigan.com>
2018-01-25 15:42:51 -0500

    organized work section

--- wp-content/themes/vega-child/template-parts/sections/work-gallery.php
+++ wp-content/themes/vega-child/template-parts/sections/work-gallery.php
@@ -9,20 +9,25 @@ $after   = $_GET['after'] ?? null;
 $gallery = new FacebookPhotoGallery();
 $albums   = $gallery->albums(12, $before, $after);
 
+$badAlbums = ['Past builds', 'Profile Pictures', 'Mobile Uploads', 'Cover Photos', 'Timeline Photos', 'Untitled Album']
 ?>
 
 <div class="row photo-gallery grid">
 	<?php
-	foreach ($albums->data as $album) { ?>
-		<div class="grid-item col-sm-6 col-md-3 mb-3">
-			<figure class="image is-4by3">
-				<a href="/our-work/?albumName=<?= $album->name ?>&albumId=<?= $album->id ?>">
-					<img src="<?= $album->cover_photo->images[4]->source ?>" alt="<?= $album->name ?>" class="img-responsive">
-				</a>
-			</figure>
-			<p class="has-text-centered"><?= $album->name ?></p>
-		</div>
-		<?php
+	foreach ($albums->data as $album) {
+	    if(!in_array($album->name,$badAlbums)) {
+		    ?>
+            <div class="grid-item col-sm-6 col-md-3 mb-3">
+                <figure class="image is-4by3">
+                    <a href="/our-work/?albumName=<?= $album->name ?>&albumId=<?= $album->id ?>">
+                        <img src="<?= $album->cover_photo->images[4]->source ?>" alt="<?= $album->name ?>"
+                             class="img-responsive">
+                    </a>
+                </figure>
+                <p class="has-text-centered"><?= $album->name ?></p>
+            </div>
+		    <?php
+	    }
 	}
 	?>
 </div>
