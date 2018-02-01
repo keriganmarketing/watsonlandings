163cb3bc0464795197295169bce0c33fc4cf50a2
Bryan Baird <bryan@kerigan.com>
2018-02-01 13:37:53 -0500

    fix empty next button

--- wp-content/themes/vega-child/template-parts/sections/work-gallery.php
+++ wp-content/themes/vega-child/template-parts/sections/work-gallery.php
@@ -6,30 +6,33 @@ $before  = $_GET['before'] ?? null;
 // Cursor after the returned data set
 $after   = $_GET['after'] ?? null;
 
-$gallery = new FacebookPhotoGallery();
-$albums   = $gallery->albums(12, $before, $after);
-
-$badAlbums = ['Past builds', 'Profile Pictures', 'Mobile Uploads', 'Cover Photos', 'Timeline Photos', 'Untitled Album']
+$gallery   = new FacebookPhotoGallery();
+$albums    = $gallery->albums(12, $before, $after);
+$badAlbums = ['Past builds', 'Profile Pictures', 'Mobile Uploads', 'Cover Photos', 'Timeline Photos', 'Untitled Album'];
+$goodAlbums = [];
+foreach ($albums->data as $album) {
+    if ( ! in_array($album->name, $badAlbums)) {
+        $goodAlbums[] = $album;
+    }
+}
+if(count($goodAlbums) <= 12){
+    unset($albums->paging->next);
+}
 ?>
 
 <div class="row photo-gallery grid">
 	<?php
-	foreach ($albums->data as $album) {
-	    if(!in_array($album->name,$badAlbums)) {
-		    ?>
-            <div class="grid-item col-sm-6 col-md-3 mb-3">
-                <figure class="image is-4by3">
-                    <a href="/our-work/?albumName=<?= $album->name ?>&albumId=<?= $album->id ?>">
-                        <img src="<?= $album->cover_photo->images[4]->source ?>" alt="<?= $album->name ?>"
-                             class="img-responsive">
-                    </a>
-                </figure>
-                <p class="has-text-centered"><?= $album->name ?></p>
-            </div>
-		    <?php
-	    }
-	}
-	?>
+	foreach ($goodAlbums as $album) { ?>
+        <div class="grid-item col-sm-6 col-md-3 mb-3">
+            <figure class="image is-4by3">
+                <a href="/our-work/?albumName=<?= $album->name ?>&albumId=<?= $album->id ?>">
+                    <img src="<?= $album->cover_photo->images[4]->source ?>" alt="<?= $album->name ?>"
+                         class="img-responsive">
+                </a>
+            </figure>
+            <p class="has-text-centered"><?= $album->name ?></p>
+        </div>
+    <?php } ?>
 </div>
 <?php
 $disabledPrevious = isset($albums->paging->previous) ? false : true;
